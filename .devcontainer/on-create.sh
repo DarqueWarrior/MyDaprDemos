235cbaac7f41d20b8bff68ec76997d9643ca4105
Donovan Brown <dlb@dlbonline.com>
2021-09-14 16:29:37 +0000

    Log into azure cli

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,5 +7,11 @@ wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O 
  
 # initialize dapr
 dapr init
+
+# log into azure cli
+az login --service-principal -t $TID -u $SPID -p $SPKEY
+
+# set the subscription
+az account set -s $SUBID
  
 echo "on-create complete" >> ~/status
