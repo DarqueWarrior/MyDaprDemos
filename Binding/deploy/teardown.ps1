a2b290238881bebfcc02712de069159f492158fd
Donovan Brown <dlb@dlbonline.com>
2021-09-15 23:24:57 +0000

    Added teardown scripts

---
+++ Binding/deploy/teardown.ps1
@@ -0,0 +1,21 @@
+# This will start the deletion of a resource group but not wait.
+
+[CmdletBinding()]
+param (
+    [Parameter(
+        Position = 0,
+        HelpMessage = "The name of the resource group to be created. All resources will be place in the resource group and start with name."
+    )]
+    [string]
+    $rgName = "bindingDemo",
+
+    [switch]
+    $force
+)
+
+if ($force.IsPresent) {
+    az group delete --resource-group $rgName --no-wait --yes
+}
+else {
+    az group delete --resource-group $rgName --no-wait
+}
