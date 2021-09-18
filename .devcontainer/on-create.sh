04bc16057e89ec236dbd6000f90cd4602038c7d3
Donovan Brown <dlb@dlbonline.com>
2021-09-18 18:14:16 +0000

    devcontainer is working

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,10 +1,7 @@
 #!/bin/bash 
  
 echo "on-create start" >> ~/status
- 
-# install dapr cli
-wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
- 
+
 # initialize dapr
 dapr init
 
@@ -19,8 +16,5 @@ az config set core.output=table
 
 # install PowerShell modules
 pwsh -Command "& {Install-Module -Name Trackyon.Utils, VSTeam -Force}"
-
-# install .net 6
-wget -q https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh -O - | /bin/bash -s - --channel 6.0.1xx --quality preview --install-dir ~/.dotnet
  
 echo "on-create complete" >> ~/status
