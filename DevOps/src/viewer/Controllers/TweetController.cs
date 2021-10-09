27c9552000303432711b39f564aad9c45eda1994
Donovan Brown <dlb@dlbonline.com>
2021-10-09 00:11:38 +0000

    added workspace for DevOps

---
+++ DevOps/src/viewer/Controllers/TweetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace viewer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TweetController : ControllerBase
+{
+    private readonly ILogger<TweetController> _logger;
+
+    public TweetController(ILogger<TweetController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    [Dapr.Topic("pubsub", "scored")]
+    public void PostTweet(object model)
+    {
+        this._logger.LogInformation("Viewer");
+    }
+}
--- DevOps/src/viewer/Program.cs
+++ DevOps/src/viewer/Program.cs
@@ -1,6 +1,12 @@
-
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
 builder.Services.AddControllers().AddDapr();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "temp", Version = "v1" });
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -11,17 +17,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "temp v1"));
 }
 else
 {
     app.UseExceptionHandler("/Error");
 }
+
 app.UseStaticFiles();
 app.UseCloudEvents();
 app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapSubscribeHandler());
 
 app.UseAuthorization();
+
+app.MapControllers();
 
 app.MapRazorPages();
 
