8a075b6b9e6cbb09c7df028f5031e2400e197332
Donovan Brown <dlb@dlbonline.com>
2021-11-03 12:33:14 +0000

    kubectl

--- .devcontainer/library-scripts/dev-tools-debian.sh
+++ .devcontainer/library-scripts/dev-tools-debian.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 
-apt-get install software-properties-common -y
+apt-get install -y software-properties-common apt-transport-https gnupg2
 
 # Install .net 6
 export DOTNET_ROOT=$HOME/dotnet
@@ -14,10 +14,16 @@ wget -q https://aka.ms/install-dotnet-preview -O - | /bin/bash -s - --install-di
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 
 # Install K3d CLI
- wget -q https://raw.githubusercontent.com/rancher/k3d/main/install.sh -O - | /bin/bash
+wget -q https://raw.githubusercontent.com/rancher/k3d/main/install.sh -O - | /bin/bash
 
- # Install terraform
- curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
- apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
- apt-get update
- apt-get install terraform -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update
+
+# Install kubectl
+apt-get install -y kubectl
+
+# Install terraform 
+apt-get install -y terraform
